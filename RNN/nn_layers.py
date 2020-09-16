d3c987c0c779ceeba349e1d49b7dd516c1f2e94b
digicope <51534708+digicope@users.noreply.github.com>
2020-09-16 14:49:08 +0900

    Add files via upload

--- RNN/nn_layers.py
+++ RNN/nn_layers.py
@@ -197,6 +197,20 @@ def softmax(x):
 
     return x
 
+class Softmax:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
 
 # def cross_entropy_error(y, t):
 #     if y.ndim == 1:
@@ -1105,4 +1119,65 @@ class TimeDropout:
     def backward(self, dout):
         return dout * self.mask
     
+# Encoder class
+class Encoder:
+    def __init__(self,vocab_size,wordvec_size,hidden_size):
+        V,D,H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        
+        embed_W = (rn(V,D)/100).astype('f')
+        lstm_Wx = (rn(D, 4*H) / np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4*H) / np.sqrt(H)).astype('f')
+        lstm_b  = np.zeros(4*H).astype('f')
+        
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=False)
+        
+        self.params = self.embed.params + self.lstm.params
+        self.grads = self.embed.grads + self.lstm.grads
+        self.hs = None
+        
+    def forward(self,xs):
+        xs = self.embed.forward(xs)
+        hs = self.lstm.forward(xs)
+        self.hs = hs
+        return hs[:,-1,:] #  마지막 TimeLSTM 계층의 은닉 상태를 반환
     
+    def backward(self,dh):
+        dhs = np.zeros_like(self.hs)
+        dhs[:,-1,:] = dh
+        
+        dout = self.lstm.backward(dhs)
+        dout = self.embed.backward(dout)
+        return dout
+
+
+class Seq2seq():
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = Decoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
+
+    def forward(self, xs, ts):
+        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+
+        h = self.encoder.forward(xs)
+        score = self.decoder.forward(decoder_xs, h)
+        loss = self.softmax.forward(score, decoder_ts)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.softmax.backward(dout)
+        dh = self.decoder.backward(dout)
+        dout = self.encoder.backward(dh)
+        return dout
+
+    def generate(self, xs, start_id, sample_size):
+        h = self.encoder.forward(xs)
+        sampled = self.decoder.generate(h, start_id, sample_size)
+        return sampled    
+             
